795b63662c704e14c5dc8114b997c62479e106f8
tiagobrilhante <codebrilhante>
2023-05-05 09:52:33 -0400

    se não tiver munição, volta pro cano... UI ajustada

--- src/entidades/interativos/TiroPlayer.java
+++ src/entidades/interativos/TiroPlayer.java
@@ -5,6 +5,7 @@ import Mundo.Camera;
 import Mundo.Mundo;
 import entidades.Entity;
 import entidades.player.Player;
+import entidades.solidos.Solido;
 import main.Game;
 
 import java.awt.*;
@@ -14,6 +15,8 @@ public class TiroPlayer extends Entity {
 
     // velocidade de deslocamento do tiro(teste)
     public double speed = 2.5;
+
+    public int[] dimensoesTiro = {30, 2};
 
     // localização
     public double xa, ya;
@@ -50,6 +53,9 @@ public class TiroPlayer extends Entity {
         y = ya - Camera.y;
         timer++;
 
+        if (colisao((int)x,(int)y)){
+            System.out.println("colidiu");
+        }
 
 
     }
@@ -58,8 +64,24 @@ public class TiroPlayer extends Entity {
     public void render(Graphics g) {
 
         g.setColor(Color.BLACK);
-        g.fillRect((int)( xa), (int)(ya), 50, 50);
+        g.fillRect((int)( xa), (int)(ya), dimensoesTiro[0], dimensoesTiro[1]);
 
+    }
+
+    public boolean colisao(int nextx, int nexty) {
+
+        Rectangle tiroRetangle = new Rectangle((int)xa, (int)ya, dimensoesTiro[0], dimensoesTiro[1]);
+
+        for (int i = 0; i < Game.entidades.size(); i++) {
+            Entity entidade = Game.entidades.get(i);
+            if (entidade instanceof Solido) {
+                Rectangle solido = new Rectangle(entidade.getX() + Entity.SIZEENTITYX, entidade.getY() + Entity.SIZEENTITYY, Entity.SIZEENTITYX, Entity.SIZEENTITYY);
+                if (tiroRetangle.intersects(solido)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 
